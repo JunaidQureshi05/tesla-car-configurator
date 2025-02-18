c6a7291c32bfbd700194c1c71d670497d6f75d6f
Junaid Qureshi <Junaid.562000@gmail.com>
2025-02-18 14:20:01 +0530

    change exterior car images

---
+++ js/main.js
@@ -0,0 +1,70 @@
+const topBar = document.querySelector("#top-bar");
+const exteriorColorSection = document.querySelector("#exterior-buttons");
+const exteriorImage = document.querySelector("#exterior-image");
+let selectedColor = "Stealth Grey";
+
+const selectedOptions = {
+  "Performance Wheels": false,
+  "Performance Package": false,
+  "Full Self-Drving": false,
+};
+
+// Image Mapping
+const exteriorImages = {
+  "Stealth Grey": "./images/model-y-stealth-grey.jpg",
+  "Pearl White": "./images/model-y-pearl-white.jpg",
+  "Deep Blue": "./images/model-y-deep-blue-metallic.jpg",
+  "Solid Black": "./images/model-y-solid-black.jpg",
+  "Ultra Red": "./images/model-y-ultra-red.jpg",
+  Quicksilver: "./images/model-y-quicksilver.jpg",
+};
+
+const interiorImages = {
+  Dark: "./images/model-y-interior-dark.jpg",
+  Light: "./images/model-y-interior-light.jpg",
+};
+function handleScroll() {
+  const atTop = window.scrollY === 0;
+  topBar.classList.toggle("visible-bar", atTop);
+  topBar.classList.toggle("hidden-bar", !atTop);
+}
+
+function updateExteriorImage() {
+  console.log("#####SELECTED COLOR ", selectedColor);
+  const performanceSuffix = selectedOptions["Performance Wheels"]
+    ? "-performance"
+    : "";
+  const colorKey =
+    selectedColor in exteriorImages ? selectedColor : "Stealth Grey";
+  exteriorImage.src = exteriorImages[colorKey].replace(
+    ".jpg",
+    `${performanceSuffix}.jpg`
+  );
+}
+
+function handleColorButtonClick(e) {
+  console.log("e.target.tagName", e.target.tagName);
+  let button;
+  if ((e.target.tagName = "IMG")) {
+    button = e.target.closest("button");
+  } else {
+    button = e.target;
+  }
+  console.log("button", button);
+  if (button) {
+    const buttons = e.currentTarget.querySelectorAll("button");
+    buttons.forEach((btn) => btn.classList.remove("btn-selected"));
+    button.classList.add("btn-selected");
+
+    // Change exterior image
+    if (e.currentTarget === exteriorColorSection) {
+      selectedColor = button.querySelector("img").alt;
+      updateExteriorImage();
+    }
+  }
+}
+
+// Event listeners
+
+window.addEventListener("scroll", () => requestAnimationFrame(handleScroll));
+exteriorColorSection.addEventListener("click", handleColorButtonClick);
